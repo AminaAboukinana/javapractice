0836f61a13cd9fb6f5b7f3c08289a7c3ab76232b
Mina <amina.aboukinana@teamaol.com>
2016-03-04 11:29:59 -0500

    "this adds arrays"

---
+++ Array.java
@@ -0,0 +1,6 @@
+class Array {
+public static void main (String[] args) {
+	int Age[]={87,62,93,105,248};
+System.out.println(Age[1]);
+}
+}
---
+++ arrayTable.java
@@ -0,0 +1,7 @@
+class arrayTable {
+public static void main (String[] args) {
+	int Age[]={87, 62, 93};
+	for (int counter=0; counter<Age.length; counter++)
+	{System.out.println (counter + "\t" + Age[counter]);}
+	}
+}
